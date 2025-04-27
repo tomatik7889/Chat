38591ddd2e66a334b578df97729d5a7b977d0d6b
tomatik7889 <bylka568@gmail.com>
2025-04-28 00:27:24 +0300

    4

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path');
-const { Socket } = require("dgram");
-const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
-const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
-const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
-const server = http.createServer((req,res)=>{
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const server = http.createServer((req, res) => {
     switch(req.url) {
-        case '/': return res.end(indexHtmlFile)
-        case '/script.js': return res.end(scriptFile)
-        case '/style.css': return res.end(styleFile)
+        case '/': return res.end(indexHtmlFile);
+        case '/script.js': return res.end(scriptFile);
+        case '/style.css': return res.end(styleFile);
     }
-    if(req.url === '/'){
-        return res.end(indexHtmlFile)
-    }
-    res.statusCode = 404;
-    return res.end('Error 404')
+    return res.end('Error 404');
 });
-server.listen(3000); 
-
-
-
-
+server.listen(3000);
+const { Server } = require("socket.io");
+const io = new Server(server);
 io.on('connection', (socket) => {
-    console.log('a user connected. id - ' + socket.id);
-    let userNickname = 'user';
-    socket.on('set_nickname', (nickname) => {
-      userNickname = nickname;
-    });
-    socket.on('new_message', (message) => {
-      io.emit('message', userNickname + ' : ' + message);
-    });
+  console.log('a user connected. id - ' + socket.id);
+  let userNickname = 'user';
+  socket.on('set_nickname', (nickname) => {
+    userNickname = nickname;
   });
-  
-  
+  socket.on('new_message', (message) => {
+    io.emit('message', userNickname + ' : ' + message);
+  });
+});
+
